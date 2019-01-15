1601c56b5b2f4f92a6a21075ad5e19cfd0f84db5
Nivelian <alex.mazur@rambler.ru>
2019-01-15 23:54:02 +0300

    did nothing

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './Game.module.scss';
-import { isEqual } from 'lodash';
-import { when } from 'utils';
+import { keymap } from 'utils';
 import { Harold } from './harold/Harold';
 
+const direction = [
+  
+]
+
+//const SPEED = 0.05;
+let _coord = f => keymap(['x', 'y'], f);
 
 class _Game extends Component {
+
   constructor (props) {
     super(props);
     this.state = {center: {x: 0, y: 0}};
   }
 
-  componentDidUpdate ({dim}) {
-    when(!isEqual(dim, this.props.dim),
-      () => this.setState({center: {x: this.props.dim.width/2, y: this.props.dim.height/2}}));
-  }
+  componentDidMount () {this.redraw()}
+
+  redraw = () => {
+    //this.setState({center: _center(this.state.center, this.mouse)});
+    requestAnimationFrame(this.redraw);
+  };
 
   render () {return (
     <div className="full anchor"> <Harold center={this.state.center} /> </div>
