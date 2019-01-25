7257904ebea1d2521bd2fc694b046566332bb2c9
Aliaksei Mazur <MazurA@iba>
2019-01-25 17:05:22 +0300

    simplified speed recalculation

--- src/game/Game.js
+++ src/game/Game.js
@@ -6,7 +6,7 @@ import { filter } from 'lodash/fp';
 import { when, let_, bool, comp, fitBounds } from 'utils';
 import { Harold } from './harold/Harold';
 
-const [MAX_SPEED, ACC] = [5, .1];
+const [MAX_SPEED, ACC] = [5, .2];
 const X_DIR = {37: -1, 39: 1};
 
 const [HAROLD_W, HAROLD_H] = [40, 40];
@@ -15,8 +15,8 @@ let _compose = comp(sum, filter(bool), toArray);
 
 class _Game extends Component {
   xDirections = new Set();
-  dir = {x: 0, y: -1};
-  speed = {x: {'+': 0, '-': 0}, y: {'+': 0, '-': 0}};
+  dir   = {x: 0, y: -1};
+  speed = {x: 0, y: 0};
   _screen;
 
   constructor (props) {
@@ -34,26 +34,25 @@ class _Game extends Component {
     when(!isEqual(dim, this.props.dim) && this.props.dim,
       o => {this._setState({max: {x: o.width - HAROLD_W, y: o.height - HAROLD_H}})})}
 
-  setY = b => {this.dir.y = (b ? 1 : -1)}
+  _setSpeed = k => let_((dir=this.dir[k], v=this.speed[k]) => {
+    let [low, high] = [this.harold[k] <= 0, this.harold[k] >= this.max[k]];
+    let diff = ACC*(dir !== 0 ? dir : -Math.sign(v)/2);
+    this.speed[k] = (v && (low || high)) ? 0 : fitBounds(-MAX_SPEED , MAX_SPEED, v+diff)});
+
+  setY = b => {this.dir.y = (b ? 1 : -1)};
   setX = (code, b) => let_((x=X_DIR[code]) => {
-    when(!(b && this.xDirections.has(x)), () => {          // avoid repetitive keydown calls
+    when(!(b && this.xDirections.has(x)), () => {
       this.xDirections[b ? 'add' : 'delete'](x);
       this.dir.x = _compose(this.xDirections);
     });
   });
   setDir = b => ({keyCode}) => (keyCode === 32 ? this.setY(b) : this.setX(keyCode, b));
 
-  _iteration = k => let_((low=this.harold[k] <= 0, high=this.harold[k] >= this.max[k]) => {
-    when(this.dir[k] ===  1, () => {this.speed[k]['+'] = high ? 0 : Math.min(MAX_SPEED, this.speed[k]['+']+ACC)});
-    when(this.dir[k] === -1, () => {this.speed[k]['-'] = low  ? 0 : Math.min(MAX_SPEED, this.speed[k]['-']+ACC)});
-    when(this.dir[k] !== -1, () => {this.speed[k]['-'] = high ? 0 : Math.max(0, this.speed[k]['-']-ACC)});
-    when(this.dir[k] !==  1, () => {this.speed[k]['+'] = low  ? 0 : Math.max(0, this.speed[k]['+']-ACC)});
-    return this.speed[k]['+']-this.speed[k]['-'];
-  });
-
   redraw = () => {
-    ['x', 'y'].forEach(k =>
-      this._setHarold({[k]: fitBounds(0, this.max[k], this.harold[k] + this._iteration(k))}));
+    ['x', 'y'].forEach(k => {
+      this._setSpeed(k);
+      this._setHarold({[k]: fitBounds(0, this.max[k], this.harold[k] + this.speed[k])});
+    });
     requestAnimationFrame(this.redraw);
   };
 
